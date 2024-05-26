03fa78567168699b340b8e3992a2544561face99
sid <sidengineer.014@gmail.com>
2024-05-27 00:42:24 +0530

    map marker added and optimized

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 const Pin = ({ item }) => {
   return (
     <Marker position={[item.latitude, item.longitude]}>
-      <Popup >
+      <Popup className="w-[250px]" >
         <div className="flex h-20 space-x-2">
-          <img src={item.img} alt="img" className="w-20 h-full object-cover " />
-          <div className="grid grid-rows-3 justify-center items-start w-30">
-            <Link to={`/${item.id}`}>{item.title}</Link>
-            <p>{item.bedroom} bedrooms</p>
-            <p>${item.price}</p>
+          <img src={item.img} alt="img" className="w-16 h-full object-cover rounded-md" />
+          <div className="grid grid-rows-3 gap-4 w-48 ">
+            <Link to={`/${item.id}`} className="w-40"  >{item.title}  </Link>
+            <span className="flex" >{item.bedroom} bedrooms</span>
+            <span className="flex" >${item.price}</span>
           </div>
         </div>
       </Popup>
