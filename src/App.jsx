beb6fa7053c8eb8cf20c1bed3a65cd05b94e8621
sid <sidengineer.014@gmail.com>
2024-06-24 16:18:02 +0530

    contact us page link change 3

--- src/App.jsx
+++ src/App.jsx
@@ -47,11 +47,11 @@ function App() {
           element: <CreatePost/>,
         },
         {
-          path: "/searching/hidden/our-story",
+          path: "/search/our-story",
           element: <About/>,
         },
         {
-          path: "/searching/hidden/contact-us",
+          path: "/search/contact-us",
           element: <ContactUs/>,
         },
       
